6326151ea258783a712b8c67691258f2a830c313
Camille Rosewright <camille.rosewright@gmail.com>
2026-05-03 18:20:14 +0900

    refactor: update directions dicts
    use enums for directions and consts for the animation dicts

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,63 +1,64 @@
 extends CharacterBody2D
 
+const SPEED: float = 300.0
 
-const speed: float = 300.0
+enum Direction {	UP, DOWN, LEFT, RIGHT}
+var current_direction: Direction = Direction.DOWN
 
-var current_direction: String = "none"
+const WALK_ANIMATIONS: Dictionary[Direction, String] = {
+	Direction.UP: "back_walk",
+	Direction.DOWN: "front_walk",
+	Direction.LEFT: "side_walk",
+	Direction.RIGHT: "side_walk",
+}
+
+const IDLE_ANIMATIONS: Dictionary[Direction, String] = {
+	Direction.UP: "back_idle",
+	Direction.DOWN: "front_idle",
+	Direction.LEFT: "side_idle",
+	Direction.RIGHT: "side_idle",
+}
+
 
 func _ready() -> void:
-	$AnimatedSprite2D.play("down_idle")
+	play_animation(get_animation_name(IDLE_ANIMATIONS))
 
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
 
 
-func player_movement(_delta: float) -> void:
-	var animation_walk_dict: Dictionary[String, String] = {
-		"up": "up_walk",
-		"down": "down_walk",
-		"left": "side_walk",
-		"right": "side_walk",
-	}
-
-	var animation_idle_dict: Dictionary[String, String] = {
-		"up": "up_idle",
-		"down": "down_idle",
-		"left": "side_idle",
-		"right": "side_idle",
-	}
-
+func player_movement(_delta: float) -> void:		
 	if Input.is_action_pressed("ui_right"):
-		current_direction = "right"
-		play_animation(get_animation_name(animation_walk_dict))
+		current_direction = Direction.RIGHT
+		play_animation(get_animation_name(WALK_ANIMATIONS))
 		
-		velocity.x = speed
+		velocity.x = SPEED
 		velocity.y = 0
 		
 	elif Input.is_action_pressed("ui_left"):
-		current_direction = "left"
-		play_animation(get_animation_name(animation_walk_dict))
+		current_direction = Direction.LEFT
+		play_animation(get_animation_name(WALK_ANIMATIONS))
 		
-		velocity.x = -speed
+		velocity.x = -SPEED
 		velocity.y = 0
 		
 	elif Input.is_action_pressed("ui_up"):
-		current_direction = "up"
-		play_animation(get_animation_name(animation_walk_dict))
+		current_direction = Direction.UP
+		play_animation(get_animation_name(WALK_ANIMATIONS))
 
 		velocity.x = 0
-		velocity.y = -speed
+		velocity.y = -SPEED
 		
 	elif Input.is_action_pressed("ui_down"):
-		current_direction = "down"
-		play_animation(get_animation_name(animation_walk_dict))
+		current_direction = Direction.DOWN
+		play_animation(get_animation_name(WALK_ANIMATIONS))
 
 		velocity.x = 0
-		velocity.y = speed
+		velocity.y = SPEED
 	
 	else:
-		play_animation(get_animation_name(animation_idle_dict))
+		play_animation(get_animation_name(IDLE_ANIMATIONS))
 		velocity.x = 0
 		velocity.y = 0
 
@@ -66,9 +67,9 @@ func player_movement(_delta: float) -> void:
 
 func play_animation(animation_name: String) -> void:
 	var animation: AnimatedSprite2D = $AnimatedSprite2D
-	animation.flip_h = current_direction == "left"
+	animation.flip_h = current_direction == Direction.LEFT
 	animation.play(animation_name)
 	
 
-func get_animation_name(direction_dict: Dictionary[String, String]) -> String:
-	return str(direction_dict.get(current_direction, "side_idle"))
+func get_animation_name(direction_dict: Dictionary[Direction, String]) -> String:
+	return str(direction_dict.get(current_direction))
